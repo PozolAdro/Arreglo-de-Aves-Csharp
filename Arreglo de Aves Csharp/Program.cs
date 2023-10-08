04cce0a4bff782628ada172d4467b1a48b301255
Adrian <Adrian@DESKTOP-COFL36B>
2023-10-07 21:34:50 -0600

    Agregar archivos de proyecto.

---
+++ Arreglo de Aves Csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        
+        List<string> aves = new List<string> { "Loro gris", "Paloma de diamante", "Coctel" };
+        Console.WriteLine("Los valores de la lista antes de insertar:");
+
+       
+        foreach (string ave in aves)
+        {
+            Console.Write(ave + " ");
+        }
+        Console.WriteLine();
+
+        
+        aves.Add("Mayna");
+        aves.Add("Periquitos");
+        aves.Add("Cacatua");
+
+        Console.WriteLine("Los valores de la lista después de insertar:");
+
+        
+        foreach (string ave in aves)
+        {
+            Console.Write(ave + " ");
+        }
+        Console.WriteLine();
+    }
+}
